aaf9961a3e44714ae7dde88e44fdc30d3ed59d01
dragonhunt02 <jackhunt43@libero.it>
2025-05-22 02:58:46 +0200

    Update node.cpp

--- godot/scene/main/node.cpp
+++ godot/scene/main/node.cpp
@@ -850,21 +850,22 @@ void Node::rpc_config(const StringName &p_method, const Variant &p_config) {
 }
 
 Variant Node::get_rpc_config(bool script_rpc_get) const {
-    if (script_rpc_get && get_script_instance()) {
+    if (script_rpc_get && get_script_instance()) { // return attached script rpc configs too
 		//ERR_FAIL_COND(data.rpc_config.get_type() != Variant::DICTIONARY);
 		const Dictionary node_config = data.rpc_config;
-		//const auto node_config = data.rpc_config;
 		const Dictionary script_config = get_script_instance()->get_rpc_config();
 		Dictionary merged_config = Dictionary();
+
 		const Array node_names = node_config.keys();
 		for (int i = 0; i < node_names.size(); i++) {
 			const auto name = node_names[i];
 			merged_config[name] = node_config[name];
 		}
+
 		const Array script_names = script_config.keys();
 		for (int i = 0; i < script_names.size(); i++) {
-			//ERR_FAIL_COND(p_config.get_type() != Variant::DICTIONARY);
 			const auto name = script_names[i];
+			ERR_FAIL_COND(merged_config.has(name));
 			merged_config[name] = script_config[name];
 		}
 		return merged_config;
@@ -4000,7 +4001,7 @@ void Node::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("get_multiplayer"), &Node::get_multiplayer);
 	ClassDB::bind_method(D_METHOD("rpc_config", "method", "config"), &Node::rpc_config);
-	ClassDB::bind_method(D_METHOD("get_rpc_config"), &Node::get_rpc_config);
+	ClassDB::bind_method(D_METHOD("get_rpc_config", "script_rpc_get"), &Node::get_rpc_config, DEFVAL(true));
 
 	ClassDB::bind_method(D_METHOD("set_editor_description", "editor_description"), &Node::set_editor_description);
 	ClassDB::bind_method(D_METHOD("get_editor_description"), &Node::get_editor_description);
